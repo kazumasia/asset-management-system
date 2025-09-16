a21327f26cc64e8418e1bb3ac3d56c132892babb
kazumasia <109078511+kazumasia@users.noreply.github.com>
2025-09-16 21:01:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const socketIo = require('socket.io');
 const express = require('express');
 
 const app = express();
-app.use(express.json()); // Untuk membaca body JSON
+app.use(express.json()); 
 
-// Buat HTTP server
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
@@ -14,31 +13,26 @@ const io = socketIo(server, {
     }
 });
 
-// Tangkap koneksi socket
 io.on("connection", (socket) => {
     console.log("User connected: " + socket.id);
 
-    // Menerima notifikasi pemeliharaan dari PHP
     socket.on("maintenance-reminder", (data) => {
         console.log("Notifikasi Pemeliharaan: ", data);
-        io.emit("maintenance-reminder", data); // Kirim ke semua user
+        io.emit("maintenance-reminder", data); 
     });
 });
 
-// Endpoint untuk menerima notifikasi dari PHP
 app.post('/send-notification', (req, res) => {
     const data = req.body;
-    io.emit('receive-notification', data); // Kirim notifikasi ke semua client
+    io.emit('receive-notification', data); 
     console.log('Notification sent:', data);
     res.status(200).send('Notification sent');
 });
 
-// Jalankan Express server di port 3001
 app.listen(3001, () => {
     console.log('Express server running on port 3001');
 });
 
-// Jalankan Socket.IO server di port 3000
 server.listen(3000, () => {
     console.log('Socket.IO server running on port 3000');
 });
